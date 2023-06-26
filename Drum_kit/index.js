0df770915e8f6869d5db8afecdfdb138950cd50c
Rakesh Kumar Thakur <86558469+rakeshrkt@users.noreply.github.com>
2023-06-26 19:58:50 +0530

    Add files via upload

---
+++ Drum_kit/index.js
@@ -0,0 +1,69 @@
+document.querySelector(".crash").addEventListener("click",function(){
+    var audio = new Audio("crash.mp3");
+    return audio.play();
+});
+
+document.querySelector(".kick").addEventListener("click",function(){
+    var audio = new Audio("kick-bass.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom1").addEventListener("click",function(){
+    var audio = new Audio("tom-1.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom2").addEventListener("click",function(){
+    var audio = new Audio("tom-2.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom3").addEventListener("click",function(){
+    var audio = new Audio("tom-3.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom4").addEventListener("click",function(){
+    var audio = new Audio("tom-4.mp3");
+    return audio.play();
+});
+
+document.querySelector(".snare").addEventListener("click",function(){
+    var audio = new Audio("snare.mp3");
+    return audio.play();
+});
+
+document.querySelector(".crash").addEventListener("click",function(){
+    var audio = new Audio("crash.mp3");
+    return audio.play();
+});
+
+document.querySelector(".kick").addEventListener("click",function(){
+    var audio = new Audio("kick-bass.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom1").addEventListener("click",function(){
+    var audio = new Audio("tom-1.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom2").addEventListener("click",function(){
+    var audio = new Audio("tom-2.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom3").addEventListener("click",function(){
+    var audio = new Audio("tom-3.mp3");
+    return audio.play();
+});
+
+document.querySelector(".tom4").addEventListener("click",function(){
+    var audio = new Audio("tom-4.mp3");
+    return audio.play();
+});
+
+document.querySelector(".snare").addEventListener("click",function(){
+    var audio = new Audio("snare.mp3");
+    return audio.play();
+});
